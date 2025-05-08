7182dc4590346989044ee721456e4de6ab4769f0
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-05-08 18:28:36 +0800

    feat: implement 2 APIs for POST and GET, and functions to convert and upload files to AWS S3

---
+++ backend/app/models/aws.py
@@ -0,0 +1,45 @@
+import boto3, os, logging
+from uuid import uuid4
+from PIL import Image
+from io import BytesIO
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def convert_to_webp(photo_file):
+    try:
+        contents = await photo_file.read()
+        image = Image.open(BytesIO(contents))
+
+        webp_image_io = BytesIO()
+        image.save(webp_image_io, format="WEBP", quality=85)
+        webp_image_io.seek(0)
+
+        return webp_image_io
+
+    except Exception as e:
+        logging.error(f"Error converting image to webp: {e}")
+        print(f"Error converting image to webp: {e}")
+        return None
+
+
+async def upload_to_s3(file: BytesIO, filename: str = "default.webp", content_type: str = "image/webp"):
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION"),
+    )
+
+    file_ext = filename.split(".")[-1]
+    key = f"posts/{uuid4()}.{file_ext}"
+    content = file.getvalue()
+    try:
+        s3.put_object(Bucket=os.getenv("AWS_BUCKET"), Key=key, Body=content, ContentType=content_type)
+        cloudfront_url = f"{os.getenv('CLOUDFRONT_DOMAIN')}/{key}"
+
+        return cloudfront_url
+    except Exception as e:
+        print(f"Error uploading to S3: {e}")
+        return None
---
+++ backend/app/routers/first_wk.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, UploadFile, Form
+from fastapi.responses import JSONResponse
+from backend.app.models.aws import convert_to_webp, upload_to_s3
+from backend.app.db.dbquery import insert_text_img_data, get_text_img_data
+
+router = APIRouter()
+
+@router.post("/api/posting")
+async def posting(text: str = Form(...), file: UploadFile = Form(...)):
+    print(file)
+    file_webp_io = await convert_to_webp(file)
+
+    if file_webp_io:
+        filename_oc = file.filename if file.filename else "default.webp"
+        webp_filename = f"{filename_oc.rsplit('.', 1)[0]}.webp"
+        webp_content_type = "image/webp"
+        
+        file_url = await upload_to_s3(file_webp_io, webp_filename, webp_content_type)
+
+        insert_text_img_data(text, file_url)
+
+        return {"text": text, "image_url": file_url}
+    else:
+        return JSONResponse(status_code=500, content={"message": "Error converting image to webp"})
+    
+
+@router.get("/api/posts")
+def get_posts():
+    try:
+
+        img_text_data = get_text_img_data()
+
+        return {
+            "data": [
+                {
+                    "id": text_img["id"],
+                    "text": text_img["msg_text"],
+                    "img_url": text_img["image_url"],
+                    "creation_time": text_img["creation_time"]
+                }
+                for text_img in img_text_data
+            ]
+        }
+    except:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "error":True,
+                "message":"伺服器錯誤..."
+                }
+                )
