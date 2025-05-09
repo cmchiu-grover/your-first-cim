fa2fd8391c0dc748dec51fd69fc5f7c4345a49b8
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-05-09 22:03:14 +0800

    feat: replace static with Docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from backend.app.routers.first_wk import router as first_week_router
+from app.routers.first_wk import router as first_week_router
 
 app = FastAPI()
 
-app.mount("/frontend/static", StaticFiles(directory="frontend/static"), name="static")
+# app.mount("/frontend/static", StaticFiles(directory="frontend/static"), name="static")
 
 app.include_router(first_week_router)
 
-@app.get("/msg_img_posts", include_in_schema=False)
-async def index(request: Request):
-    return FileResponse("frontend/static/msg_img_posts.html", media_type="text/html")
+# @app.get("/msg_img_posts", include_in_schema=False)
+# async def index(request: Request):
+#     return FileResponse("frontend/static/msg_img_posts.html", media_type="text/html")
