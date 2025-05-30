2109edb821bb429affef1eda4bde201a919cc974
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-05-30 22:11:28 +0800

    feat: backend updated to create the tables

--- backend/app/db/tables.py
+++ backend/app/db/tables.py
@@ -38,22 +38,176 @@ TABLES['users'] = (
     "  INDEX `index_account_position` (`account`, `position`)"
     ") ENGINE=InnoDB")
 
-TABLES['eqp_status_test'] = (
-    "CREATE TABLE `eqp_status_test` ("
+TABLES['eqp_types'] = (
+    "CREATE TABLE `eqp_types` ("
     "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
-    "  `eq_id` VARCHAR(16) NOT NULL,"
-    "  `work_date` datetime NOT NULL,"
-    "  `start_time` datetime NOT NULL,"
-    "  `end_time` datetime NOT NULL,"
-    "  `hour_time` DECIMAL(10,6) NOT NULL,"
-    "  `status` VARCHAR(16) NOT NULL,"
-    "  PRIMARY KEY (`id`), "
-    "  INDEX `index_work_date` (`work_date`),"
-    "  INDEX `index_start_time` (`start_time`),"
-    "  INDEX `index_eqid_date` (`eq_id`, `work_date`),"
-    "  INDEX `index_eqid_starttime` (`eq_id`, `start_time`),"
-    "  INDEX `index_status` (`status`)"
+    "  `eqp_type` VARCHAR(10) UNIQUE NOT NULL,"
+    "  `description` VARCHAR(255),"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`)"
     ") ENGINE=InnoDB")
+
+TABLES['station_info'] = (
+    "CREATE TABLE `station_info` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `module_name` VARCHAR(50) NOT NULL,"
+    "  `station_name` VARCHAR(50) UNIQUE NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX `index_module_name` (`module_name`),"
+    "  INDEX `index_module_station_name` (`module_name`, `station_name`)"
+    ") ENGINE=InnoDB")
+
+TABLES['status_types'] = (
+    "CREATE TABLE `status_types` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `status_name` VARCHAR(50) UNIQUE NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['product_categories'] = (
+    "CREATE TABLE `product_categories` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `category_name` VARCHAR(50) UNIQUE NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['prod_info'] = (
+    "CREATE TABLE `prod_info` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `prod_code` VARCHAR(16) UNIQUE NOT NULL,"
+    "  `prod_name` VARCHAR(100) NOT NULL,"
+    "  `category_id` INT(11) NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX `index_prod_name` (`prod_name`),"
+    "  UNIQUE INDEX `idx_prod_category_name_code` (`category_id`, `prod_name`, `prod_code`),"
+    "  FOREIGN KEY (`category_id`) REFERENCES `product_categories`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['standard_times'] = (
+    "CREATE TABLE `standard_times` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `prod_id` INT(11) NOT NULL,"
+    "  `eqp_type_id` INT(11) NOT NULL,"
+    "  `station_id` INT(11) NOT NULL,"
+    "  `standard_time_value` DECIMAL(10,4) NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX `index_eqp_type_id` (`eqp_type_id`),"
+    "  INDEX `index_station_id` (`station_id`),"
+    "  UNIQUE INDEX `idx_unique_standard_time` (`prod_id`, `eqp_type_id`, `station_id`),"
+    "  FOREIGN KEY (`prod_id`) REFERENCES `prod_info`(`id`),"
+    "  FOREIGN KEY (`eqp_type_id`) REFERENCES `eqp_types`(`id`),"
+    "  FOREIGN KEY (`station_id`) REFERENCES `station_info`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['eqp_info'] = (
+    "CREATE TABLE `eqp_info` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `eqp_code` VARCHAR(16) UNIQUE NOT NULL,"
+    "  `eqp_type_id` INT(11) NOT NULL,"
+    "  `station_id` INT(11) NOT NULL,"
+    "  `location` VARCHAR(100) NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX `index_station_id` (`station_id`),"
+    "  UNIQUE INDEX `idx_eqp_code_station_unique` (`eqp_code`, `station_id`),"
+    "  FOREIGN KEY (`eqp_type_id`) REFERENCES `eqp_types`(`id`),"
+    "  FOREIGN KEY (`station_id`) REFERENCES `station_info`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['eqp_status'] = (
+    "CREATE TABLE `eqp_status` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `eqp_id` INT(11) NOT NULL,"
+    "  `work_date` DATE NOT NULL,"
+    "  `start_time` DATETIME NOT NULL,"
+    "  `end_time` DATETIME NULL,"
+    "  `hours` DECIMAL(10,6) NULL,"
+    "  `status_id` INT(11) NOT NULL,"
+    "  `comment` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  PRIMARY KEY (`id`),"
+    "  INDEX `index_work_date` (`work_date`),"
+    "  INDEX `index_eqp_id` (`eqp_id`),"
+    "  INDEX `index_status_id` (`status_id`),"
+    "  INDEX `index_start_time` (`start_time`),"
+    "  INDEX `idx_eqp_id_start_time` (`eqp_id`, `start_time`),"
+    "  FOREIGN KEY (`eqp_id`) REFERENCES `eqp_info`(`id`),"
+    "  FOREIGN KEY (`status_id`) REFERENCES `status_types`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['product_routings'] = (
+    "CREATE TABLE `product_routings` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `prod_id` INT(11) NOT NULL,"
+    "  `station_id` INT(11) NOT NULL,"
+    "  `sequence_number` VARCHAR(3) NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  UNIQUE INDEX `idx_prod_station` (`prod_id`, `station_id`),"
+    "  PRIMARY KEY (`id`),"
+    "  FOREIGN KEY (`prod_id`) REFERENCES `prod_info`(`id`),"
+    "  FOREIGN KEY (`station_id`) REFERENCES `station_info`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['work_orders'] = (
+    "CREATE TABLE `work_orders` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  `prod_id` INT(11) NOT NULL,"
+    "  `qty` INT(11) NOT NULL,"
+    "  `order_status` VARCHAR(50) NOT NULL,"
+    "  `description` VARCHAR(255) NULL,"
+    "  `due_date` DATETIME NULL,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  INDEX `idx_prod_id` (`prod_id`),"
+    "  PRIMARY KEY (`id`),"
+    "  FOREIGN KEY (`prod_id`) REFERENCES `prod_info`(`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['notifications'] = (
+    "CREATE TABLE `notifications` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  event_type VARCHAR(50),"
+    "  title VARCHAR(255),"
+    "  message TEXT,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  INDEX `idx_event_type` (`event_type`),"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['user_notifications'] = (
+    "CREATE TABLE `user_notifications` ("
+    "  `id` INT(11) NOT NULL AUTO_INCREMENT,"
+    "  user_id INT,"
+    "  notification_id INT,"
+    "  is_read BOOLEAN DEFAULT FALSE,"
+    "  `creation_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+    "  `updated_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,"
+    "  INDEX `idx_user_id` (`user_id`),"
+    "  PRIMARY KEY (`id`),"
+    "  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),"
+    "  FOREIGN KEY (notification_id) REFERENCES notifications(id)"
+    ") ENGINE=InnoDB")
+
 
 def check_database():
     try:
