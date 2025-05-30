3b3a9e16284d2e01f480d3990c0e93392f95e8cd
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-05-30 22:41:33 +0800

    feat: backend created a router to use SSE

---
+++ backend/app/routers/sse.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import StreamingResponse
+from backend.app.db.crud import get_current_active_user
+from backend.app.db.dbquery import check_unread_notifications
+import asyncio
+import redis.asyncio as redis
+
+router = APIRouter()
+r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+@router.get("/sse/standard_time")
+async def sse_standard_time(request: Request):
+    async def event_generator():
+        pubsub = r.pubsub()
+        await pubsub.subscribe("standard_time_channel")
+
+        try:
+            while True:
+                if await request.is_disconnected():
+                    break
+                message = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1.0)
+                if message:
+                    data = message["data"]
+                    yield f"data: {data}\n\n"
+                await asyncio.sleep(0.5)
+        except asyncio.CancelledError:
+            print("SSE connection cancelled.")
+        except Exception as e:
+            print(f"Error in SSE: {e}")
+            yield f"data: {{'error': 'An error occurred: {str(e)}'}}\n\n"
+        finally:
+            await pubsub.unsubscribe("standard_time_channel")
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+@router.get("/api/notifications/unread")
+async def get_unread_notifications(request: Request):
+    try:
+        user = await get_current_active_user(request)
+        print(f"get_unread_notifications: {user}")
+        
+        rows = check_unread_notifications(int(user.get('id')))
+        if not rows:
+            print("沒有未讀通知")
+            return {"has_unread": False}
+        
+        else:
+            print("有未讀通知")
+            return {"has_unread": rows}
+    except Exception as e:
+        print(f"get_unread_notifications 發生錯誤：{e}")
+        return {"error": str(e)}
+    
