405fdbbed34c01b1e449327e4da7f6c44fd4f2af
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-06-11 14:59:46 +0800

    feat: FE dashboard page added fetch OEE data

--- frontend/static/js/dashboard.js
+++ frontend/static/js/dashboard.js
@@ -107,3 +107,63 @@ async function main() {
 main();
 
 signout();
+
+document.addEventListener("DOMContentLoaded", async function () {
+  function updateOEETable(apiData) {
+    if (!apiData.ok || !Array.isArray(apiData.data)) return;
+
+    apiData.data.forEach((item) => {
+      const station = item.metrics;
+      console.log(station);
+
+      const oeeCell = document.querySelector(
+        `#oeeRow td[data-station="${station}"]`
+      );
+      console.log(oeeCell);
+      if (oeeCell) {
+        oeeCell.textContent = item.oee_rate.toFixed(2);
+        oeeCell.classList.toggle("low-value", item.oee_rate < 72.0);
+      }
+
+      const availCell = document.querySelector(
+        `#availRow td[data-station="${station}"]`
+      );
+      if (availCell) {
+        availCell.textContent = item.avail_rate.toFixed(2);
+        availCell.classList.toggle("low-value", item.avail_rate < 80.0);
+      }
+
+      const perfCell = document.querySelector(
+        `#perfRow td[data-station="${station}"]`
+      );
+      if (perfCell) {
+        perfCell.textContent = item.perf_rate.toFixed(2);
+        perfCell.classList.toggle("low-value", item.perfCell < 90.0);
+      }
+    });
+  }
+
+  try {
+    const response = await fetch("/api/oee/yesterday", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        errorData.detail || `HTTP error! status: ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+    console.log("查詢結果:", data);
+    updateOEETable(data);
+  } catch (error) {
+    console.error("查詢失敗:", error);
+
+    window.alert(`查詢失敗: ${error.message}`);
+  }
+});
