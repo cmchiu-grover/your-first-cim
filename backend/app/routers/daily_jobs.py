1c8d2777d6bc83ebba876f216153374203207420
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-06-12 23:24:53 +0800

    feat: BE add updated OEE

--- backend/app/routers/daily_jobs.py
+++ backend/app/routers/daily_jobs.py
@@ -4,7 +4,7 @@ from backend.app.models.daily_oee_moving import process_oee_data
 from backend.app.models.generate_eqp_status import generate_eqp_status
 from backend.app.models.generate_gantt_chart import generate_gantt_chart
 from backend.app.models.generate_eqp_wip import generate_eqp_wip
-from backend.app.models.generate_temp_oee import generate_temp_oee
+from backend.app.models.updated_temp_oee import generate_temp_oee
 
 scheduler = AsyncIOScheduler(timezone="Asia/Taipei")
 
@@ -26,21 +26,21 @@ def start_daily_jobs():
 
     scheduler.add_job(
         generate_eqp_wip,
-        CronTrigger(hour=22, minute=57),
+        CronTrigger(hour=6, minute=39),
         id="generate_eqp_wip_job",
         replace_existing=True
     )
 
     scheduler.add_job(
         generate_temp_oee,
-        CronTrigger(hour=22, minute=59),
+        CronTrigger(hour=23, minute=26),
         id="generate_temp_oee_job",
         replace_existing=True
     )
 
     scheduler.add_job(
         process_oee_data,
-        CronTrigger(hour=23, minute=1),
+        CronTrigger(hour=6, minute=45),
         id="daily_oee_job",
         replace_existing=True
     )
